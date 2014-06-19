cf5871150145bff51c0f737154f54b59a581501f
wang0678 <xinyangw8562@gmail.com>
2014-06-19 18:22:01 +0930

    This is initial prac3 testcase

---
+++ test/au/edu/flinders/uni/cs/se3/prac3/TestGame2048.java
@@ -0,0 +1,13 @@
+package au.edu.flinders.uni.cs.se3.prac3;
+
+import junit.framework.TestCase;
+
+/**
+ * This is the Junit TestGame2048 class which is used to test Game2048Core class
+ * for Prac 3
+ * 
+ */
+public class TestGame2048 extends TestCase {
+
+	
+}
